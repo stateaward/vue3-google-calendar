f65562274ae936a05cde16c38947df5619858a00
stateaward <stateaward@naver.com>
2021-09-21 21:44:02 +0900

    fix: 🐛 eslint setup-1

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 2. Manually select features
 3. Bable, TypeScript, Linter, CSS-preprocessor ...
 
+[참고블로그](https://hasudoki.tistory.com/entry/Vuejs-Vue-3-TypeScript-2-%EC%84%B8%ED%8C%85%ED%95%98%EA%B8%B0Setting-Up-Vue-3-TypeScript)
+
+---
+
 ## Project setup
 
 ```
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,13 @@
+// Vue 컴포넌트 정보를 타입스크립트에 제공하는 파일
+
 /* eslint-disable */
+// declare module '*.vue' {
+//    import Vue from 'vue';
+//    export default Vue;
+// }
+
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+   import type { DefineComponent } from 'vue';
+   const component: DefineComponent<{}, {}, any>;
+   export default component;
 }
